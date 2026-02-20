b9cc169d09b66c33a590ce1dac61cfdb712d89f8
ArtemB887 <artem.burdun@e-u.edu.ua>
2026-02-20 13:04:26 +0200

    Practical 3: staircase

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,31 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+
+pub fn staircase(n: i32) -> String {
+    let mut result = String::new();
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+        result.push_str(&spaces);
+        result.push_str(&hashes);
+        if i < n {
+            result.push('\n');
+        }
+    }
+    result
+}
+
+#[allow(dead_code)]
+pub fn solve(n: i32) {
+    println!("{}", staircase(n));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_staircase() {
+        let expected = "   #\n  ##\n ###\n####";
+        assert_eq!(staircase(4), expected);
+    }
+}
