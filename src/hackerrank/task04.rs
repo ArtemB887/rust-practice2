3df07821c81793e846beaefa2c6e5d1fdca91e13
ArtemB887 <artem.burdun@e-u.edu.ua>
2026-02-21 11:01:58 +0200

    Practical 4: Grading Students

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,36 @@
 // https://www.hackerrank.com/challenges/grading/problem
+
+pub fn grading_students(grades: &[i32]) -> Vec<i32> {
+    grades.iter().map(|&grade| {
+        if grade < 38 {
+            grade
+        } else {
+            let next_multiple_of_5 = ((grade / 5) + 1) * 5;
+            if next_multiple_of_5 - grade < 3 {
+                next_multiple_of_5
+            } else {
+                grade
+            }
+        }
+    }).collect()
+}
+
+#[allow(dead_code)]
+pub fn solve(grades: &[i32]) {
+    let result = grading_students(grades);
+    for grade in result {
+        println!("{}", grade);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_grading_students() {
+        let input = [73, 67, 38, 33];
+        let expected = [75, 67, 40, 33];
+        assert_eq!(grading_students(&input), expected);
+    }
+}
