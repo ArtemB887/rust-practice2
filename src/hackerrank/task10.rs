39159d1975c05e9f40b3fa10e51f089619ffc032
ArtemB887 <artem.burdun@e-u.edu.ua>
2026-03-10 13:35:28 +0200

    Practical 10: Sales by Match

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,24 @@
-// https://www.hackerrank.com/challenges/sock-merchant/problem
+pub fn sock_merchant(_n: i32, ar: Vec<i32>) -> i32 {
+    let mut counts = [0; 101];
+    let mut pairs = 0;
+
+    for sock in ar {
+        counts[sock as usize] += 1;
+        if counts[sock as usize] % 2 == 0 {
+            pairs += 1;
+        }
+    }
+
+    pairs
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sock_merchant() {
+        assert_eq!(sock_merchant(9, vec![10, 20, 20, 10, 10, 30, 50, 10, 20]), 3);
+        assert_eq!(sock_merchant(10, vec![1, 1, 3, 1, 2, 1, 3, 3, 3, 3]), 4);
+    }
+}
